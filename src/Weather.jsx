87976c02e0644f8450cdc1408d94117fac4c87e5
Phu Le <phu3262006@gmail.com>
2024-11-07 23:45:19 -0500

    set up react

---
+++ src/Weather.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+export default function Weather(){
+
+    return(
+        <div className="container">
+            <div className="weather-report">
+                
+            </div>
+        </div>
+    )
+}
